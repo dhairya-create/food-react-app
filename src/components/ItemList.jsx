dec85d5b1afcbf878f2a1aa685f8e9cd5b2553a4
dhairya-create <dhairyaparikh769@gmail.com>
2024-03-05 13:42:11 +0530

    Add button added

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -11,20 +11,33 @@ const ItemList = ({ items }) => {
           >
             <div className="flex justify-between items-center ">
               <div className="flex items-center">
-                <span className="font-bold mx-1">{item.card.info.name + " "} </span>
+                <span className="font-bold mx-1">
+                  {item.card.info.name + " "}{" "}
+                </span>
                 <div className="font-bold">
-                ₹{item.card.info.price ? item.card.info.price / 100 : item.card.info.defaultPrice/100}
+                  ₹
+                  {item.card.info.price
+                    ? item.card.info.price / 100
+                    : item.card.info.defaultPrice / 100}
                 </div>
               </div>
 
-              <img
-                src={
-                  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/" +
-                  item?.card?.info?.imageId
-                }
-                alt="res-logo"
-                className="w-16 mb-2"
-              />
+              <div className="w-3/12 p-4">
+                <div className="absolute">
+                  <button className="p-1 mx-0 rounded-lg bg-black opacity-75 text-white">
+                    Add +
+                  </button>
+                </div>
+
+                <img
+                  src={
+                    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/" +
+                    item?.card?.info?.imageId
+                  }
+                  alt="res-logo"
+                  className="w-30 mb-2"
+                />
+              </div>
             </div>
             <p className="text-sm text-gray-400">
               {item.card.info.description}
